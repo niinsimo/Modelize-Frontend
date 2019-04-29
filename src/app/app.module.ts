bf60f287cc1c4938f213422de50d47d0e13c0ba5
Simone Niinemägi <31885412+niinsimo@users.noreply.github.com>
2019-04-29 10:27:10 +0300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCheckboxModule, MatMenuModule, MatIconModule } from '@angular/material';
+import { TopMenuComponent } from './top-menu/top-menu.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TopMenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule, 
+    MatCheckboxModule,
+    MatMenuModule,
+    MatIconModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
