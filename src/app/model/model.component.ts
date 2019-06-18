d01a7c71363a71913979fe6d8f4f18ee9b72817e
Marten Rebane <St3althCmD@users.noreply.github.com>
2019-06-19 01:13:00 +0300

    GLTF loader

--- src/app/model/model.component.ts
+++ src/app/model/model.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, ViewChild, ElementRef, HostListener } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import * as THREE from 'three';
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
+import GLTFLoader from 'three-gltf-loader';
 
 @Component({
   selector: 'app-model',
@@ -7,42 +9,71 @@ import * as THREE from 'three';
   styleUrls: ['./model.component.scss']
 })
 
-export class ModelComponent {
+export class ModelComponent implements OnInit, OnDestroy {
 
-  @ViewChild('rendererContainer', { static: true }) rendererContainer: ElementRef;
-  @HostListener('window:resize', ['$event'])
-  onWindowResize(event: { target: { innerWidth: number; innerHeight: number; }; }) {
-      this.renderer.setSize(event.target.innerWidth, event.target.innerHeight)
+  domElement: HTMLElement;
+
+  ngOnInit(): void {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera( 25, window.innerWidth / window.innerHeight, 1, 20000 );
+    camera.position.set( 1, 1, 20 );
+    const renderer = new THREE.WebGLRenderer({ alpha: false });
+    renderer.setClearColor( 0xC5C5C3 );
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    this.domElement = renderer.domElement
+    document.body.appendChild(this.domElement);
+
+    scene.background = new THREE.Color('grey');
+    scene.add(camera);
+
+    // Load Light
+    const ambientLight = new THREE.AmbientLight( 0xcccccc );
+    scene.add( ambientLight );
+
+    const directionalLight = new THREE.DirectionalLight( 0xffffff );
+    directionalLight.position.set( 0, 1, 1 ).normalize();
+    scene.add( directionalLight );
+
+    const loader = new GLTFLoader();
+    loader.load(
+      '', // Resource location (assets folder)
+      function ( gltf ) {
+        console.log(gltf);
+
+        const object = gltf.scene;
+        object.scale.set( 4, 4, 4 );
+        object.position.x = 0;	// Position (x = right+ left-)
+        object.position.y = 0;	// Position (y = up+, down-)
+        object.position.z = 0;
+
+        scene.add( object );
+
+        renderer.render( scene, camera );
+
+      },
+      // Called while loading is progressing
+      function ( xhr ) {
+
+        console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+      },
+      // Called when loading has errors
+      function ( error ) {
+
+        scene.dispose();
+        document.body.removeChild(renderer.domElement);
+
+        throw Error(error.message);
+
+      }
+    );
   }
 
-  renderer = new THREE.WebGLRenderer();
-    scene = null;
-    camera = null;
-    mesh = null;
-
-    constructor() {
-        this.scene = new THREE.Scene();
-
-        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
-        this.camera.position.z = 1000;
-
-        const geometry = new THREE.BoxGeometry(200, 200, 200);
-        const material = new THREE.MeshBasicMaterial({color: 0xff0000, wireframe: true});
-        this.mesh = new THREE.Mesh(geometry, material);
-
-        this.scene.add(this.mesh);
+  ngOnDestroy() {
+    if (document.body.contains(this.domElement)) {
+      document.body.removeChild(this.domElement);
     }
+  }
 
-    ngAfterViewInit() {
-        this.renderer.setSize(window.innerWidth, window.innerHeight);
-        this.rendererContainer.nativeElement.appendChild(this.renderer.domElement);
-        this.animate();
-    }
-
-    animate() {
-        window.requestAnimationFrame(() => this.animate());
-        this.mesh.rotation.x += 0.01;
-        this.mesh.rotation.y += 0.02;
-        this.renderer.render(this.scene, this.camera);
-    }
 }
