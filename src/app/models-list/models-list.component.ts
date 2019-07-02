5e417dcb569cca29b25a672135b8b3571259155d
Simone Niinemägi <31885412+niinsimo@users.noreply.github.com>
2019-07-02 16:39:52 +0300

    Updated models list

--- src/app/models-list/models-list.component.ts
+++ src/app/models-list/models-list.component.ts
@@ -1,30 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-models-list',
   templateUrl: './models-list.component.html',
   styleUrls: ['./models-list.component.scss']
 })
-export class ModelsListComponent implements OnInit {
-  imageCollection = [];
+export class ModelsListComponent {
 
-  constructor() { 
-    for (let i = 0; i < 9; i++) {
-      //const url = 'http://via.placeholder.com/400x300?text=Image No ' + (i + 1);
-      const images = ['https://cdn.dribbble.com/users/673247/screenshots/2500770/atom.gif', 'https://i.giphy.com/media/rVz1J8spLtUtO/giphy.webp', 
-      'https://media.giphy.com/media/PZ2XgGsn4n49q/giphy.gif', 'https://media.giphy.com/media/OGdWZnVzRP1IY/giphy.gif',
-      'https://media.giphy.com/media/tqxqW5F4RtdWo/giphy.gif', 'https://media.giphy.com/media/87dO7wL8mTYzK/giphy.gif',
-      'https://media.giphy.com/media/11i03wSzIuNXiw/giphy.gif', 'https://media2.giphy.com/media/Ufvzy251HTbQA/giphy.gif?cid=790b76115d10f90b595571425986180c&rid=giphy.gif',
-      'https://media3.giphy.com/media/ZFiNY3IDiP9UA/giphy.gif?cid=790b76115d10f900746e7579551a5478&rid=giphy.gif'];
+  images = ['https://cdn.dribbble.com/users/673247/screenshots/2500770/atom.gif', 'https://i.giphy.com/media/rVz1J8spLtUtO/giphy.webp', 
+  'https://media.giphy.com/media/PZ2XgGsn4n49q/giphy.gif', 'https://media.giphy.com/media/OGdWZnVzRP1IY/giphy.gif',
+  'https://media.giphy.com/media/tqxqW5F4RtdWo/giphy.gif', 'https://media.giphy.com/media/87dO7wL8mTYzK/giphy.gif',
+  'https://media.giphy.com/media/11i03wSzIuNXiw/giphy.gif', 'https://media2.giphy.com/media/Ufvzy251HTbQA/giphy.gif?cid=790b76115d10f90b595571425986180c&rid=giphy.gif',
+  'https://media3.giphy.com/media/ZFiNY3IDiP9UA/giphy.gif?cid=790b76115d10f900746e7579551a5478&rid=giphy.gif'];
 
-      this.imageCollection[i] = {
-        url: images[i],
-        show: false
-      };
-    }
-  }
-
-  ngOnInit() {
+  drop(event: CdkDragDrop<string[]>) {
+    //moveItemInArray(this.images, event.previousIndex, event.currentIndex);
   }
 
 }
